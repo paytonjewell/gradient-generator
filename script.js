9d807c44f6292a593e0c8b4a3d0a74880832d8f8
Payton Jewell <paytontaylor96@gmail.com>
2020-01-29 13:26:30 -0600

    Made default input values match initial load gradient. Made gradient code display in h3 on page load. Created randomizer button.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+window.onload = setGradient;
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
@@ -9,6 +12,21 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomRGB() {
+	var rgbValues = [];
+	for(var i = 0; i < 3; i++) {
+		rgbValues.push(Math.floor(Math.random() * 256));
+	}
+	return 'rgb(' + rgbValues[0] + ',' + rgbValues[1] + ',' + rgbValues[2] + ')';
+}
+
+function setRandom() {
+	body.style.background = 'linear-gradient(to right, ' + randomRGB() + ', ' + randomRGB() + ')';
+	css.textContent = body.style.background + ';';
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandom);
